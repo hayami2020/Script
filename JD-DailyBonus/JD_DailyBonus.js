a54298f332b337c669a0c2b6961fcebf3fa1d07f
hayami2020 <66722061+hayami2020@users.noreply.github.com>
2021-09-10 14:44:37 +0800

    Update JD_DailyBonus.js

--- JD-DailyBonus/JD_DailyBonus.js
+++ JD-DailyBonus/JD_DailyBonus.js
@@ -46,7 +46,8 @@ var Key = ''; //该参数已废弃; 仅用于下游脚本的兼容, 请使用jso
 
 var DualKey = ''; //该参数已废弃; 仅用于下游脚本的兼容, 请使用json串数据  ↓
 
-var OtherKey = ``; //无限账号Cookie json串数据, 请严格按照json格式填写, 具体格式请看以下样例:
+var OtherKey = `[{pt_key=AAJhOHqeADC1EetHzySeLbd794VUAccODF3vogBeXIak6Z5cy856h_XFGCdThG-wW2XRjN1wy9A;pt_pin=hayami2011;"
+}]; //无限账号Cookie json串数据, 请严格按照json格式填写, 具体格式请看以下样例:
 
 /*以下样例为双账号("cookie"为必须,其他可选), 第一个账号仅包含Cookie, 第二个账号包含Cookie和金融签到Body: 
 
